27a635940a4bcf084be4f7c95dd43ccb8d44ff41
BungaDoR <oktaviab14@gmail.com>
2025-09-27 16:13:26 +0700

    buat icon stopwatch

--- lib/tugas/quiz_1.dart
+++ lib/tugas/quiz_1.dart
@@ -56,8 +56,14 @@ class quiz_1 extends StatelessWidget {
                           Row(
                             mainAxisAlignment: MainAxisAlignment.spaceBetween,
                             children: [
-                              Text("60 min",
+                              Container(
+                                width: 10, height: 10,
+                                color: Colors.white,
+                                child: 
+                                Icon(Icons.alarm, color: Colors.white,),
+                               Text("60 min",
                               style: TextStyle(color: Colors.white),
+                              ),
                               ),
                               Container(
                                 width: 40, height: 40,
